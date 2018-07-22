34525590da8cdca9ba7d1e90e9312c77568b0e07
krakissi <mike@krakissi.net>
2018-07-21 21:04:07 -0400

    Fix casino role name

--- ewcfg.py
+++ ewcfg.py
@@ -1725,7 +1725,7 @@ poi_list = [
 		str_desc = "The casino is filled with tables and machines for playing games of chance, and garishly decorated wall-to-wall. Lights which normally flash constantly cover everything, but now they all sit unlit.",
 		coord = (29, 16),
 		channel = channel_casino,
-		role = "The Casino",
+		role = "Casino",
 		pvp = False
 	)
 ]
