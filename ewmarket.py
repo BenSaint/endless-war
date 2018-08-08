5850a368647490741c45155b3a6a0877af48f610
Ben Saint <38568511+BenSaint@users.noreply.github.com>
2018-08-08 00:02:17 -0400

    re-added Invest
    invest slime for Slimecoin, exchange rate 1000/1

--- ewmarket.py
+++ ewmarket.py
@@ -74,6 +74,59 @@ async def xfer(cmd):
 	# Send the response to the player.
 	await cmd.client.edit_message(resp, ewutils.formatMessage(cmd.message.author, response))
 
+""" transfer slimecoin between players """
+async def invest(cmd):
+	resp = await ewcmd.start(cmd = cmd)
+	time_now = int(time.time())
+
+	if cmd.message.channel.name != ewcfg.channel_stockexchange:
+		# Only allowed in the stock exchange.
+		response = ewcfg.str_exchange_channelreq.format(currency = "slime", action = "invest")
+		await cmd.client.edit_message(resp, ewutils.formatMessage(cmd.message.author, response))
+		return
+
+	user_data = EwUser(member = cmd.message.author)
+	market_data = EwMarket(id_server = cmd.message.author.server.id)
+
+	if market_data.clock > 19 or market_data.clock < 6:
+		response = ewcfg.str_exchange_closed
+	else:
+		# Parse the slime value to send.
+		value = None
+		if cmd.tokens_count > 1:
+			value = ewutils.getIntToken(tokens = cmd.tokens, allow_all = True)
+
+		if value != None:
+			if value < 0:
+				value = user_data.slimes
+			if value <= 0:
+				value = None
+
+		if value != None:
+			# Amount of slime invested.
+			cost_total = (int(value / 1000) * 1000)
+			coin_total = int(value / 1000)
+
+			if user_data.slime < cost_total:
+				response = "You don't have that much slime. ({:,}/{:,})".format(user_data.slimes, cost_total)
+			elif user_data.time_lastinvest + ewcfg.cd_invest > time_now:
+				# Limit frequency of investments.
+				response = ewcfg.str_exchange_busy.format(action = "invest")
+			else:
+				# Do the transfer if the player can afford it.
+				user_data.slimes -= cost_total
+				user_data.slimecredit += coin_total
+				user_data.time_lastinvest = time_now
+
+				# Persist changes
+				response = "You exchange {slime:,} slime for {slimecoin:,} SlimeCoin.".format(slime = cost_total, slimecoin = coin_total)
+
+				user_data.persist()
+		else:
+			response = ewcfg.str_exchange_specify.format(currency = "slime", action = "invest")
+
+	# Send the response to the player.
+	await cmd.client.edit_message(resp, ewutils.formatMessage(cmd.message.author, response))
 
 """ show player's slimecoin balance """
 async def slimecoin(cmd):
