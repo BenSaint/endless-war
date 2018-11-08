c5335559dd0fdf9a5b8baf75daab5bebdcddd976
hardchlor <31193666+hardchlor@users.noreply.github.com>
2018-11-08 22:37:45 +0100

    started working on districts

---
+++ ewdistrict.py
@@ -0,0 +1,13 @@
+"""
+	district data model for database persistence
+"""
+class EwDistrict:
+	id_server = ""
+	name = ""
+	controlling_faction = ""
+	capture_progress = 0
+
+
+
+def capture_tick():
+	i = 0
