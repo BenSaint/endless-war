a7c816822d468ec664ba71b408784f77cd97af97
krakissi <mike@krakissi.net>
2018-12-30 10:05:47 -0500

    bump rev v2.13a

--- ewcfg.py
+++ ewcfg.py
@@ -9,7 +9,7 @@ from ewmap import EwPoi
 from ewslimeoid import EwBody, EwHead, EwMobility, EwOffense, EwDefense, EwSpecial, EwBrain
 
 # Global configuration options.
-version = "v2.13"
+version = "v2.13a"
 dir_msgqueue = 'msgqueue'
 
 # Update intervals
